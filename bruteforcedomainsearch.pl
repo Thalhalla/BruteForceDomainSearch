dd52dce97267744947184911bb31d4ed0f257280
Josh Cox <Josh.Cox@ipsoft.com>
2014-04-30 14:44:20 -0500

    ws

--- bruteforcedomainsearch.pl
+++ bruteforcedomainsearch.pl
@@ -25,9 +25,7 @@ while( $n <= $limit ){
     my @chars = qw( a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 );
     push(@chars, '-');
     my @n = @chars;
-    my $combinat = Math::Combinatorics->new(count => $n,
-                                          data => [@n],
-                                         );
+    my $combinat = Math::Combinatorics->new(count => $n, data => [@n], );
 
     while(my @combo = $combinat->next_combination){
         $domain_word = '';
